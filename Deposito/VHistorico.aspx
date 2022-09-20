d51fe3dca511384c81fb3bc1a99238237c4151f1
HPSCOMPUTOS <soporte.hpasoria@gmail.com>
2022-09-20 17:46:10 -0300

    sql historico

---
+++ Deposito/VHistorico.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Deposito/MasterDeposito.Master" AutoEventWireup="true" CodeBehind="VHistorico.aspx.cs" Inherits="Sistema_Integral_HPS.Deposito.VHistorico" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label ID="Label1" class="h1 justify-content-center" runat="server">HISTORICO DE PRECIOS DE PRODUCTOS EN ADQUISICIONES</asp:Label>
+    <hr />
+    <asp:Label ID="Label2" runat="server" Text="INGRESE EL NOMBRE DEL ARTICULO A BUSCAR:" Visible="True"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="BUSCAR" />
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+</asp:Content>
---
+++ Deposito/VHistorico.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Sistema_Integral_HPS.Deposito
+{
+    public partial class VHistorico : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection coon = Conexion.getConexion();
+            MySqlCommand cm = new MySqlCommand("SELECT articulo.descripcion AS Nombre, articulo.descripcion_adicional AS Descripcion, historial_precio.fk_adquisicion AS N°Adquisicion, historial_precio.precio AS PRECIO, historial_precio.fecha AS Dia_Adquisicion  FROM `historial_precio` INNER JOIN articulo ON articulo.id=historial_precio.fk_articulo  WHERE fk_articulo LIKE '%" + TextBox1.Text + "%' ORDER BY historial_precio.fecha", coon);
+            cm.CommandType = CommandType.Text;
+            cm.ExecuteNonQuery();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+            if (GridView1.Rows.Count == 0)
+            {
+                string msg = "ARTÍCULO SIN EXISTENCIA!";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Alerta", "alert('" + msg + "');", true);
+            }
+
+            coon.Close();
+        }
+    }
+}
