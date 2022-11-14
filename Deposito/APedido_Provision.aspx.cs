31dbca8f6a77b78742e193ebcc31d57a6f0991c0
HPSCOMPUTOS <soporte.hpasoria@gmail.com>
2022-11-14 10:19:24 -0300

    10.19

--- Deposito/APedido_Provision.aspx.cs
+++ Deposito/APedido_Provision.aspx.cs
@@ -185,7 +185,7 @@ namespace Sistema_Integral_HPS.Deposito
             coon.Close();
 
             string msg = "PEDIDO DE CAJA CHICA ID " + idped + " REALIZADO";
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "Alerta", "alert('" + msg + "'); window.location = '/Sistema_Integral_HPS/Deposito/IndexDeposito.aspx\r\n';", true);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Alerta", "alert('" + msg + "'); window.location = '/Sistema_Integral_HPS/Deposito/IndexDeposito.aspx';", true);
 
                         //Response.Redirect("/Deposito/IndexDeposito.aspx");
         }
